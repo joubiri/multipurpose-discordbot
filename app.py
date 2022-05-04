1fb0ee7d4e0887b3aa1640a21c6fa365c288c572
joubiri <achraf.joubiti@usmba.ac.ma>
2022-05-04 22:03:31 +0000

    update

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import json
 import random
 import urllib.parse
 import requests
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 client = discord.Client()
 
+#API to extract COVID data based on user input
 def covidtracker(country):
 	link="https://covid-19.dataflowkit.com/v1/"+country
 	covid=requests.get(link)
@@ -28,6 +31,25 @@ def covidtracker(country):
 	covidresult= "Country: "+namecountry+".\n"+"Last Update: "+lastupdate+".\n"+"Active Cases: "+activecases+".\n"+"Total Cases: "+totalcases+".\n"+"New Cases: "+newcases+".\n"+"Total Deaths: "+totaldeaths+".\n"+"New Deaths: "+newdeaths+".\n"+"Total Recovered: "+totalrecovered+"."
 	return covidresult
 
+#web scraping module to extract last updated spreadsheet from school website
+def lastemploi():
+	links=[]
+	url = "https://ensaf.ac.ma/?controller=pages&action=emplois"
+	req = Request(url , headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'})
+	var1=urlopen(req)
+	rawhtml=var1.read()
+	scrape=soup(rawhtml,'lxml')
+	info=scrape.find('div',class_='table-responsive')
+	test=info.tbody
+	auto=test.tr
+	module=auto.select_one("tr td:nth-of-type(3)")
+	for link in module.find_all('a'):
+		autourl=link.get('href')
+	links.append(autourl)
+	return links[-1]
+
+#API for math equations
+
 def mathcalculator(parse):
 	link="http://api.mathjs.org/v4/?expr="+parse
 	results=requests.get(link)
@@ -35,6 +57,7 @@ def mathcalculator(parse):
 	calcul="Le resultat du calcul est "+str(data)
 	return calcul
 
+#API for weather data
 def get_weather(City):
 	link="http://api.openweathermap.org/data/2.5/weather?q="+str(City)+"&appid="+apikey+"&units=metric"
 	weather=requests.get(link)
@@ -74,10 +97,16 @@ async def on_message(message):
     	print(parse)
     	calcul=mathcalculator(parse)
     	await message.channel.send(calcul)
+
     if message.content.startswith('$covid'):
     	await message.channel.send("Which Country?")
     	msg2= await client.wait_for('message',timeout=30)
     	country=msg2.content
     	covid=covidtracker(country)
     	await message.channel.send(covid)
+
+    if message.content.startswith('$emploi'):
+    	lien= lastemploi()
+    	await message.channel.send(lien)
+
 client.run(TOKEN)
